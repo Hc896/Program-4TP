f9482d4b5d0689200eae1ffa14dc65dfebbf3912
DESKTOP-TR54MOT\4TP <ok>
2025-04-07 09:34:23 +0200

    30

---
+++ Nowocin/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+KontoBankowe konto = new("Anna", 1000);
+konto.WplataSaldo(500);
+konto.WyplataSaldo(200);
+Console.WriteLine(konto.PokazSaldo());
+
+class KontoBankowe
+{
+    private string wlasciciel;
+    private double saldo;
+
+    public KontoBankowe(string wlasciciel, double saldo)
+    {
+        this.wlasciciel = wlasciciel;
+        this.saldo = saldo;
+    }
+
+    public double PokazSaldo()
+    {
+        return saldo;
+    }
+
+    public void WplataSaldo(double kwota)
+    {
+        if (kwota > 0)
+            saldo += kwota;
+    }
+
+    public bool WyplataSaldo(double kwota)
+    {
+        if (kwota > 0 && kwota <= saldo)
+        {
+            saldo -= kwota;
+            return true;
+        }
+        return false;
+    }
+}
